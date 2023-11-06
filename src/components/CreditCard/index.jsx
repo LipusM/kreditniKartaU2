c8100217a06a9a5dfdf3a4c2390c5ab654af61e1
LipusM <mlipus007@gmail.com>
2023-11-06 19:21:52 +0100

    Zarovnání

--- src/components/CreditCard/index.jsx
+++ src/components/CreditCard/index.jsx
@@ -1,21 +1,20 @@
-const c = console.log.bind(document)
+const c = console.log.bind(document);
 
-import './style.scss'
+import "./style.scss";
 
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from "react";
 
 const CreditCard = () => {
-
-    return (
-        <div id='credit-card-inputs'>
-            <div>
-            <input type="text" maxLength="4"/>
-            <input type="text" maxLength="4"/>
-            <input type="text" maxLength="4"/>
-            <input type="text" maxLength="4"/>
-            </div>
-        </div>
-    )
+  return (
+    <div id="credit-card-inputs">
+      <div>
+        <input type="text" maxLength="4" />
+        <input type="text" maxLength="4" />
+        <input type="text" maxLength="4" />
+        <input type="text" maxLength="4" />
+      </div>
+    </div>
+  )
 }
 
-export default CreditCard
+export default CreditCard;
