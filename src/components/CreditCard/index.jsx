d786693f6a6b3f6ba8d3a3fa4cd4bfb08ec0c167
LipusM <mlipus007@gmail.com>
2023-11-06 19:54:22 +0100

    Vzor

--- src/components/CreditCard/index.jsx
+++ src/components/CreditCard/index.jsx
@@ -4,16 +4,31 @@ import "./style.scss";
 
 import { useState, useRef, useEffect } from "react";
 
-const pokus = () => {
-    c("hello world!")
-}
-
 const CreditCard = () => {
+
+    const firstInputRef = useRef()
+    const secondInputRef = useRef()
+
+    /* const pokus = () => {
+        c(firstInputRef.current.value.length)
+    } */
+
+    const changeFocus = () => {
+        if(firstInputRef.current.value.length === 4){
+            secondInputRef.current.focus()
+        }
+    }
+
+    useEffect(() => {
+        firstInputRef.current.focus()
+    }, [])
+
   return (
-    <form id="credit-card-inputs" onChange={pokus}>
+    <form id="credit-card-inputs" onChange={changeFocus}>
+    {/* <form id="credit-card-inputs" onChange={pokus}> */}
       <div>
-        <input type="text" maxLength="4" />
-        <input type="text" maxLength="4" />
+        <input ref={firstInputRef} type="text" maxLength="4" />
+        <input ref={secondInputRef} type="text" maxLength="4" />
         <input type="text" maxLength="4" />
         <input type="text" maxLength="4" />
       </div>
