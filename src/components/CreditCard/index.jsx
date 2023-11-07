9339fa2dbe4a2441833dd1e274618dc27cf0c787
LipusM <mlipus007@gmail.com>
2023-11-07 18:29:18 +0100

    Přepsáno na kontrolu konkrétního inputu

--- src/components/CreditCard/index.jsx
+++ src/components/CreditCard/index.jsx
@@ -1,36 +1,34 @@
-const c = console.log.bind(document);
+const c = console.log.bind(document)
 
-import "./style.scss";
+import "./style.scss"
 
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react"
 
 const CreditCard = () => {
-  const firstInputRef = useRef();
-  const secondInputRef = useRef();
-  const thirdInputRef = useRef();
-  const fourthInputRef = useRef();
+  const firstInputRef = useRef()
+  const secondInputRef = useRef()
+  const thirdInputRef = useRef()
+  const fourthInputRef = useRef()
 
-  const changeFocus = () => {
-    if (firstInputRef.current.value.length === 4) {
-      secondInputRef.current.focus();
-    } 
-    
-    if (secondInputRef.current.value.length === 4) {
-      thirdInputRef.current.focus();
-    } 
-    
-    if (thirdInputRef.current.value.length === 4) {
-      fourthInputRef.current.focus();
-    } 
+  const changeFocus = (e) => {
+    const currentInput = e.target
+  
+    if (currentInput === firstInputRef.current && currentInput.value.length === 4) {
+      secondInputRef.current.focus()
+    } else if (currentInput === secondInputRef.current && currentInput.value.length === 4) {
+      thirdInputRef.current.focus()
+    } else if (currentInput === thirdInputRef.current && currentInput.value.length === 4) {
+      fourthInputRef.current.focus()
+    }
+  
   }
 
   useEffect(() => {
-    firstInputRef.current.focus();
-  }, []);
+    firstInputRef.current.focus()
+  }, [])
 
   return (
     <form id="credit-card-inputs">
-    {/* <form id="credit-card-inputs" onChange={changeFocus}> */}
       <div>
         <input ref={firstInputRef} type="text" maxLength="4" onInput={changeFocus}/>
         <input ref={secondInputRef} type="text" maxLength="4" onInput={changeFocus}/>
